be75c1d77c6c93014244cce2aa8a7bc4698b445d
Derek Su <derek.su@suse.com>
2022-05-31 13:42:57 +0800

    dataconn: reduce write and read syscalls
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/dataconn/wire.go
+++ pkg/dataconn/wire.go
@@ -6,6 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"unsafe"
+)
+
+const (
+	requestHeaderSize = 26
 )
 
 type Wire struct {
@@ -23,24 +28,30 @@ func NewWire(conn net.Conn) *Wire {
 }
 
 func (w *Wire) Write(msg *Message) error {
-	if err := binary.Write(w.writer, binary.LittleEndian, msg.MagicVersion); err != nil {
+	header := make([]byte, requestHeaderSize)
+	offset := 0
+
+	binary.LittleEndian.PutUint16(header[offset:], msg.MagicVersion)
+	offset += int(unsafe.Sizeof(msg.MagicVersion))
+
+	binary.LittleEndian.PutUint32(header[offset:], msg.Seq)
+	offset += int(unsafe.Sizeof(msg.Seq))
+
+	binary.LittleEndian.PutUint32(header[offset:], msg.Type)
+	offset += int(unsafe.Sizeof(msg.Type))
+
+	binary.LittleEndian.PutUint64(header[offset:], uint64(msg.Offset))
+	offset += int(unsafe.Sizeof(msg.Offset))
+
+	binary.LittleEndian.PutUint32(header[offset:], msg.Size)
+	offset += int(unsafe.Sizeof(msg.Size))
+
+	binary.LittleEndian.PutUint32(header[offset:], uint32(len(msg.Data)))
+
+	if _, err := w.writer.Write(header); err != nil {
 		return err
 	}
-	if err := binary.Write(w.writer, binary.LittleEndian, msg.Seq); err != nil {
-		return err
-	}
-	if err := binary.Write(w.writer, binary.LittleEndian, msg.Type); err != nil {
-		return err
-	}
-	if err := binary.Write(w.writer, binary.LittleEndian, msg.Offset); err != nil {
-		return err
-	}
-	if err := binary.Write(w.writer, binary.LittleEndian, msg.Size); err != nil {
-		return err
-	}
-	if err := binary.Write(w.writer, binary.LittleEndian, uint32(len(msg.Data))); err != nil {
-		return err
-	}
+
 	if len(msg.Data) > 0 {
 		if _, err := w.writer.Write(msg.Data); err != nil {
 			return err
@@ -55,29 +66,32 @@ func (w *Wire) Read() (*Message, error) {
 		length uint32
 	)
 
-	if err := binary.Read(w.reader, binary.LittleEndian, &msg.MagicVersion); err != nil {
+	header := make([]byte, requestHeaderSize)
+	offset := 0
+
+	if _, err := io.ReadFull(w.reader, header); err != nil {
 		return nil, err
 	}
 
+	msg.MagicVersion = binary.LittleEndian.Uint16(header[offset:])
 	if msg.MagicVersion != MagicVersion {
 		return nil, fmt.Errorf("wrong API version received: 0x%x", &msg.MagicVersion)
 	}
+	offset += int(unsafe.Sizeof(msg.MagicVersion))
 
-	if err := binary.Read(w.reader, binary.LittleEndian, &msg.Seq); err != nil {
-		return nil, err
-	}
-	if err := binary.Read(w.reader, binary.LittleEndian, &msg.Type); err != nil {
-		return nil, err
-	}
-	if err := binary.Read(w.reader, binary.LittleEndian, &msg.Offset); err != nil {
-		return nil, err
-	}
-	if err := binary.Read(w.reader, binary.LittleEndian, &msg.Size); err != nil {
-		return nil, err
-	}
-	if err := binary.Read(w.reader, binary.LittleEndian, &length); err != nil {
-		return nil, err
-	}
+	msg.Seq = binary.LittleEndian.Uint32(header[offset:])
+	offset += int(unsafe.Sizeof(msg.Seq))
+
+	msg.Type = binary.LittleEndian.Uint32(header[offset:])
+	offset += int(unsafe.Sizeof(msg.Type))
+
+	msg.Offset = int64(binary.LittleEndian.Uint64(header[offset:]))
+	offset += int(unsafe.Sizeof(msg.Offset))
+
+	msg.Size = binary.LittleEndian.Uint32(header[offset:])
+	offset += int(unsafe.Sizeof(msg.Size))
+
+	length = binary.LittleEndian.Uint32(header[offset:])
 	if length > 0 {
 		msg.Data = make([]byte, length)
 		if _, err := io.ReadFull(w.reader, msg.Data); err != nil {
